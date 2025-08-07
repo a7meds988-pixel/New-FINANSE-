f5dc85966083b2c941c7c4cbb255403eb9dfbfbd
a7meds988-pixel <a7med.s988@gmail.com>
2025-08-07 23:34:13 +0200

    Add files via upload

---
+++ src/card.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Card({ children, className = "" }) {
+  return <div className={`rounded-xl border bg-white shadow ${className}`}>{children}</div>;
+}
+
+export function CardContent({ children, className = "" }) {
+  return <div className={`p-4 ${className}`}>{children}</div>;
+}
