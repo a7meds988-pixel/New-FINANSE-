3e877a77602a7e7be08ee490ff4da6dd1dd0b104
a7meds988-pixel <a7med.s988@gmail.com>
2025-08-07 23:26:25 +0200

    Add files via upload

---
+++ button.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export const Button = React.forwardRef(({ className = "", children, ...props }, ref) => {
+  return (
+    <button
+      ref={ref}
+      className={\`bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition-colors disabled:opacity-50 ${className}\`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+});
+
+Button.displayName = "Button";
